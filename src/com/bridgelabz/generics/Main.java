0200f4e1d93dc38faba7bf5feed14f285ed521cd
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-24 22:42:20 +0530

    Refactored : To Create Generics Class To Take 3 var in Generic Type

--- src/com/bridgelabz/generics/Main.java
+++ src/com/bridgelabz/generics/Main.java
@@ -3,11 +3,11 @@ package com.bridgelabz.generics;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to check max of three variables");
-		String x = "jack", y = "king", z = "queen", m = "rama";
-		Integer a = 26, b = 24, c = 18;
-		Float p = 1.24f, q = 2.25f, r = 0.1f;
-		TestMaximum.<String>testMaximum(x, y, z, m);
+		System.out.println("Welcome to Check maximum of three variables");
+		String x = "jack", y = "king", z = "queen";
+		Integer a = 25, b = 24, c = 19;
+		Float p = 1.23f, q = 2.2f, r = 0.2f;
+		TestMaximum.<String>testMaximum(x, y, z);
 		TestMaximum.<Integer>testMaximum(a, b, c);
 		TestMaximum.<Float>testMaximum(p, q, r);
 	}
