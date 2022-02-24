513c3e27d2c7510652bd32dc0314714a393ede27
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-24 21:05:25 +0530

    Added : Maximum of Three Integers

---
+++ src/com/bridgelabz/generics/Main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.generics;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to check max of three variables");
+
+		Integer a = 25, b = 24, c = 19;
+
+		TestMaximum.testMaximum(a, b, c);
+
+	}
+
+}
