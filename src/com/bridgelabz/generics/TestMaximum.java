709948255f045a730dd0a5765deb62b1112d81ac
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-24 22:20:13 +0530

    Refactored : All three Variables in One Generic & Find Maximum

--- src/com/bridgelabz/generics/TestMaximum.java
+++ src/com/bridgelabz/generics/TestMaximum.java
@@ -1,34 +1,36 @@
-/**UC3 : PROBLEM STATEMENT
- * Find maximum of three Strings values
- *Eg :Apple,Peach,Bnanana
+/**UC4 : PROBLEM STATEMENT
+ * Refactor all the 3 to One Generic Method and find the maximum
  *
  */
 package com.bridgelabz.generics;
 
-public class TestMaximum {
-	String x, y, z;
+public class TestMaximum<T extends Comparable<T>> {
+	private T x, y, z;
 
-	// constructor
-	public TestMaximum(String x, String y, String z) {
+//constructor
+	public TestMaximum(T x, T y, T z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
-	// checking max of three integers
-	public static String testMaximum(String x, String y, String z) {
-		String max = x;// initialising x as greater
-		if (y.compareTo(max) > 0)
-			max = y;// as of now y is greater
-
-		if (z.compareTo(max) > 0)
-			max = z;// now z is greater
-		printMax(x, y, z, max);
+//checking max of three Generic datas 
+	public static <T extends Comparable<T>> T testMaximum(T... values) {
+		T max = values[0];
+		for (int i = 1; i < values.length; i++) {
+			if (values[i].compareTo(max) > 0) {
+				max = values[i];
+			}
+		}
+		System.out.println("the maximum of following variables");
+		for (int i = 0; i < values.length; i++) {
+			System.out.print(values[i] + "\t");
+		}
+		printMax(max);
 		return max;
-
 	}
 
-	public static void printMax(String x, String y, String z, String max) {
-		System.out.printf("The maximum from string of %s , %s and %s is %s\n", x, y, z, max);
+	public static <T> void printMax(T max) {
+		System.out.println("is\t" + max);
 	}
 }
